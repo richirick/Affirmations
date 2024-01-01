0be360e12915d64b0926dd24e851d86cada82af5
richirick <113588596+richirick@users.noreply.github.com>
2024-01-01 14:58:20 +0300

    aff

--- app/src/test/java/com/example/affirmations/AffirmationsAdapterTests.kt
+++ app/src/test/java/com/example/affirmations/AffirmationsAdapterTests.kt
@@ -8,9 +8,11 @@ import org.junit.Test
 import org.mockito.Mockito.mock
 
 class AffirmationsAdapterTests {
+
     private val context = mock(Context::class.java)
+
     @Test
-    fun adapter_size(){
+    fun adapter_size() {
         val data = listOf(
             Affirmation(R.string.affirmation1, R.drawable.image1),
             Affirmation(R.string.affirmation2, R.drawable.image2)
@@ -18,5 +20,4 @@ class AffirmationsAdapterTests {
         val adapter = ItemAdapter(context, data)
         assertEquals("ItemAdapter is not the correct size", data.size, adapter.itemCount)
     }
-
 }
